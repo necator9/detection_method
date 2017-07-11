b7b4352fe99c2a0b5c03c61779884e15f02e0f12
imatveev <ivan.matveev@hs-anhalt.de>
2017-07-11 17:03:00 +0200

    Problem with v4l detected.

--- bbb_bg_subtractor/extensions.py
+++ bbb_bg_subtractor/extensions.py
@@ -6,8 +6,61 @@ from numpy import mean
 import config
 import os
 import glob
+import argparse
 
 logger = logging.getLogger(__name__)
+
+
+def parse():
+    parser = argparse.ArgumentParser(description='Motion detection setup for Beaglebone Black',
+                                     epilog='Hochschule Anhalt, 2017')
+    parser.add_argument('--ui', action='store_false',
+                        help='Disable command line interface')
+    parser.add_argument('--save', action='store_false',
+                        help='Disable image saving')
+    parser.add_argument('--sync', action='store_false',
+                        help='Disable sync with workstation')
+    parser.add_argument('--dev', metavar='/path/', default=config.DEVICE,
+                        help='Input camera device or file')
+    parser.add_argument('--ores', metavar='int', nargs=2, type=int, default=config.ORIG_IMG_RES,
+                        help='Resolution of an image to capture from camera (width and height)')
+    parser.add_argument('--pres', metavar='int', nargs=2, type=int, default=config.PROC_IMG_RES,
+                        help='Resolution of an image for processing (resize, process and save)')
+    parser.add_argument('--ofps', metavar='int', type=int, default=config.FPS,
+                        help='FPS of a camera')
+    parser.add_argument('--fkernel', metavar='int', nargs=2, type=int, default=config.F_KERNEL_SIZE,
+                        help='Size of elliptical filtering kernel in pixels')
+    parser.add_argument('--osize', metavar='int', type=int, default=config.D_OBJ_SIZE,
+                        help='Object size to be detected')
+    parser.add_argument('--bsync', metavar='/path/', default=config.BBB_SYNC_DIR,
+                        help='Path to synchronizing directory on Beaglebone Black')
+    parser.add_argument('--bsave', metavar='/path/', default=config.BBB_IMG_DIR,
+                        help='Path to image saving directory on Beaglebone Black')
+    parser.add_argument('--wsync', metavar='/path/', default=config.W_SYNC_DIR,
+                        help='Path to synchronizing directory on workstation')
+    parser.add_argument('--wuip', metavar='username@ip', default=config.W_USER_IP,
+                        help='Workstation username@ip')
+    parser.add_argument('--wport', metavar='int', default=config.W_PORT,
+                        help='Workstation ssh port')
+    args = parser.parse_args()
+
+    if not args.ui:
+        config.UI = False
+    if not args.save:
+        config.IMG_SAVE = False
+    if not args.sync:
+        config.SYNC = False
+    config.DEVICE = args.dev
+    config.ORIG_IMG_RES = args.ores
+    config.PROC_IMG_RES = args.pres
+    config.FPS = args.ofps
+    config.F_KERNEL_SIZE = args.fkernel
+    config.D_OBJ_SIZE = args.osize
+    config.BBB_SYNC_DIR = args.bsync
+    config.BBB_IMG_DIR = args.bsave
+    config.W_SYNC_DIR = args.wsync
+    config.W_USER_IP = args.wuip
+    config.W_PORT = args.wport
 
 
 class Display(threading.Thread):
