94035398eaf2d1e4bc9acf04fa37bcbb89cf8ff3
imatveev <ivan.matveev@hs-anhalt.de>
2017-09-22 18:18:37 +0200

    Drawing bag fixed, metadata writer added, tested

--- workstation_bg_subtractor/config.py
+++ workstation_bg_subtractor/config.py
@@ -1,26 +1,17 @@
 
-# Shared vars
-IMG_BUFF = []                   # Grabber writes into the buff/detector reads from
-
 # Camera parameters
 DEVICE = "/dev/video0"          # Path to input camera device or video file
 
-ORIG_IMG_RES = (640, 480)       # Width and height of an image to capture
-FPS = 30                        # Capturing frequency (frames per second)
-
 # Detector parameters
 PROC_IMG_RES = (320, 240)        # Width and height of an image for processing
-F_KERNEL_SIZE = (4, 5)          # Size of elliptical filtering kernel in pixels
-D_OBJ_SIZE = 1000                # Detection threshold. Minimal obj size to be detected
+F_KERNEL_SIZE = (4, 4)          # Size of elliptical filtering kernel in pixels
+D_OBJ_SIZE = 1500               # Detection threshold. Minimal obj size to be detected
 
 
 # Global variables for internal usage
 MOTION_STATUS = False            # Initial status of motion
 
-
-IMG_SAVE = False
-IMG_DISPLAY = True
-
-
+IMG_IN_DIR = "/home/ivan/test_ir/share/img/"
+IMG_OUT_DIR = "/home/ivan/test_ir/detection/"
 
 
--- workstation_bg_subtractor/detection.py
+++ workstation_bg_subtractor/detection.py
@@ -7,6 +7,8 @@ import cv2
 from imutils import resize
 import config
 import glob
+import pyexiv2
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -14,65 +16,91 @@ logger = logging.getLogger(__name__)
 class Detector(threading.Thread):
     def __init__(self, stop_ev):
         super(Detector, self).__init__(name="Detector")
-        # Thread state status flag
         self.running = False
         self.stop_event = stop_ev
 
         self.mog = cv2.createBackgroundSubtractorMOG2()
         self.filtering_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, config.F_KERNEL_SIZE)
         self.counter = 0
+        self.img = []
 
     # Main thread routine
     def run(self):
         logger.info("Grabber started")
         self.running = True
-        print self.counter
-        print len(glob.glob("/home/ivan/test_ir/img/*.jpeg"))
-        while (self.counter < ((len(glob.glob("/home/ivan/test_ir/share/img/*.jpeg"))) - 1)) and self.running:
 
-            start_time = time.time()
+        while (self.counter < ((len(glob.glob(os.path.join(config.IMG_IN_DIR, "*.jpeg")))) - 1)) and self.running:
+
             logger.debug("Taking image...")
 
-            image = cv2.imread(glob.glob("/home/ivan/test_ir/share/img/img_%s_*.jpeg" % self.counter)[0])
-            img = copy.copy(image)
-            img = resize(img, width=config.PROC_IMG_RES[0], height=config.PROC_IMG_RES[1])
-            mask = self.mog.apply(img)
+            self.img = cv2.imread(glob.glob(os.path.join(config.IMG_IN_DIR, "img_%s_*.jpeg" % self.counter))[0])
+            self.img = resize(self.img, width=config.PROC_IMG_RES[0], height=config.PROC_IMG_RES[1])
+            mask = self.mog.apply(self.img)
             filtered = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.filtering_kernel)
             filled = cv2.dilate(filtered, None, iterations=8)
             _, cnts, _ = cv2.findContours(filled, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            flag, in_arr, contour_area = self.detect(cnts)
-            if flag:
-                config.MOTION_STATUS = True
-                logging.info("Motion detected")
-                for i in in_arr:
-                    (x, y, w, h) = cv2.boundingRect(cnts[i])
-                    print float(h)/w
-                    print contour_area, "area size"
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 1)
-                    cv2.putText(img, str(contour_area), (x + 5, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
-                                (0, 0, 255), 1, cv2.LINE_AA)
-                    # cv2.putText(img, "w = %s" % str(w), (10, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
-                    #             (0,50, 200), 1, cv2.LINE_AA)
-                    # cv2.putText(img, "h = %s" % str(h), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
-                    #             (0, 50, 200), 1, cv2.LINE_AA)
-                    # cv2.putText(img, "h/w = %s" % str(float(h)/w), (100, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
-                    #             (0, 50, 200), 1, cv2.LINE_AA)
-                # cv2.drawContours(img, cnts, -1, (0,255,0), 3)
-            # else:
-            #     config.MOTION_STATUS = False
 
-            processing_t = time.time() - start_time
-            logger.debug("Image shooting takes %s s", processing_t)
+            det_res = self.detect(cnts)
 
-            cv2.imshow('image', img)
+            if config.MOTION_STATUS:
+                self.draw_on_img(det_res)
+
+            cv2.imshow('image', self.img)
             cv2.waitKey(1)
-            # cv2.imwrite("/home/ivan/test_ir/img_%s.jpeg" % self.counter, img)
+
+            self.save_image(det_res)
 
             self.counter += 1
             time.sleep(0.2)
-            #print self.counter
 
         self.quit()
+
+    @staticmethod
+    def detect(cnts):
+        detect_res = []
+        for arr in cnts:
+            contour_area = cv2.contourArea(arr)
+            if contour_area > config.D_OBJ_SIZE:
+                coord = cv2.boundingRect(arr)
+                detect_res.append([contour_area, coord])
+                config.MOTION_STATUS = True
+                logging.info("Motion detected")
+                logger.info(str(detect_res))
+            else:
+                config.MOTION_STATUS = False
+        return detect_res
+
+    def draw_on_img(self, det_res):
+        for i in range(len(det_res)):
+            x, y, w, h = det_res[i][1]
+            cv2.rectangle(self.img, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            cv2.putText(self.img, str(det_res[i][0]), (x + 5, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+                        (0, 0, 255), 1, cv2.LINE_AA)
+            # cv2.putText(img, "w = %s" % str(w), (10, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+            #             (0,50, 200), 1, cv2.LINE_AA)
+            # cv2.putText(img, "h = %s" % str(h), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+            #             (0, 50, 200), 1, cv2.LINE_AA)
+            # cv2.putText(img, "h/w = %s" % str(float(h)/w), (100, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.4,
+            #             (0, 50, 200), 1, cv2.LINE_AA)
+            # cv2.drawContours(img, cnts, -1, (0,255,0), 3)
+
+    def save_image(self, det_res):
+        # Save JPEG with proper name
+        img_name = "img_%s.jpeg" % self.counter
+        path = os.path.join(config.IMG_OUT_DIR, img_name)
+        cv2.imwrite(path, self.img)
+
+        # Write exif to saved JPEG
+        metadata = pyexiv2.ImageMetadata(path)
+        metadata.read()
+        metadata['Exif.Image.Software'] = pyexiv2.ExifTag('Exif.Image.Software', 'OpenCV-3.2.0-dev, pyexiv2')
+        metadata['Exif.Image.Artist'] = pyexiv2.ExifTag('Exif.Image.Artist', 'Ivan Matveev')
+        metadata['Exif.Photo.UserComment'] = pyexiv2.ExifTag('Exif.Photo.UserComment', "status:%s data:%s" %
+                                                             (config.MOTION_STATUS, det_res))
+
+        metadata['Exif.Photo.UserComment'] = pyexiv2.ExifTag('Exif.Photo.UserComment', 'status:%s' %
+                                                             config.MOTION_STATUS)
+        metadata.write()
 
     # Stop and quit the thread operation.
     def quit(self):
@@ -80,17 +108,7 @@ class Detector(threading.Thread):
         self.stop_event.clear()
         logger.info("Grabber has quit")
 
-    @staticmethod
-    def detect(cnts):
-        contour_area = 0
-        in_arr = []
-        flag = False
-        for i, arr in enumerate(cnts):
-            contour_area = cv2.contourArea(arr)
-            if contour_area > config.D_OBJ_SIZE:
-                in_arr.append(i)
-                flag = True
-        return flag, in_arr, contour_area
+
 
 
 
