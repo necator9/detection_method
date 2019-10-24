042cbef96a3d33f449de11b2aa0dea8836507de7
Ivan Matveev <ivan.matveev@hs-anhalt.de>
2019-10-24 09:31:51 +0200

    generate synth recovered old approach

--- gather_synth_data.py
+++ gather_synth_data.py
@@ -31,32 +31,36 @@ x_range = np.arange(-8, 8, 2)
 y_range = np.arange(-2, -7, -0.3)
 z_range = np.arange(1, 30, 1)
 rotate_y_angle_range = np.arange(0, 90, 22)
-thr_range = np.linspace(70, 90, 3)
-
-xyz_gen = itertools.product(x_range, y_range, z_range)
-it_params = itertools.product(xyz_gen, cam_angle, dimensions, rotate_y_angle_range, thr_range)
-
+thr_range = np.linspace(70, 90, 2)
+iter_params = list(itertools.product(x_range, y_range, z_range, thr_range))
 data = []
 
+lens = np.prod([len(i) for i in (hh_range, cam_angle, x_range, y_range, z_range, rotate_y_angle_range,
+                                 thr_range)])
+print "total iterations: {}".format(lens)
 it = 0
 
 try:
-    for (x, y, z), angle, dim, rotate_y_angle, thr in it_params:
+    for angle in cam_angle:
         pinhole_cam = PinholeCameraModel(rw_angle=angle, f_l=40., w_ccd=36., h_ccd=26.5, img_res=[424, 240])
-        obj = scale_to_size(dim, obj)
-        obj_size = [obj[:, i].max() - obj[:, i].min() for i in range(3)]
-        obj = center_obj(obj)
-        kernel_size = get_kernel_size(rotate_y_angle, (1, 13))
+        for dim in dimensions:
+            for rotate_y_angle in rotate_y_angle_range:
+                obj = scale_to_size(dim, obj)
+                obj_size = [obj[:, i].max() - obj[:, i].min() for i in range(3)]
+                obj = center_obj(obj)
+                kernel_size = get_kernel_size(rotate_y_angle, (1, 13))
 
-        m_o_vert = rotate_y(np.copy(obj), rotate_y_angle, (x, y, z))
+                for x, y, z, thr in iter_params:
+                    m_o_vert = rotate_y(np.copy(obj), rotate_y_angle, (x, y, z))
 
-        params = find_obj_params4(m_o_vert, faces, y, pinhole_cam, rotate_y_angle, thr)
-        if params is not None:
-            data.append(params + [x, y, z, rotate_y_angle] + obj_size + [angle] + [thr])
+                    params = find_obj_params4(m_o_vert, faces, y, pinhole_cam, rotate_y_angle, thr)
+                    if params != None:
+                        data.append(params + [x, y, z, rotate_y_angle] + obj_size + [angle] + [thr])
 
-        it += 1
-        if it % 100 == 0:
-            print it
+                    it += 1
+                    if it % 100 == 0:
+                        print '{:10.2f}, {}\n'.format(it / lens * 100, it)
+
 
 except KeyboardInterrupt:
     pass
