e938ec05c787a9c455d13b108f83318a82e48997
Ivan Matveev <ivan.matveev@hs-anhalt.de>
2019-11-13 12:00:25 +0100

    Working callibration

--- camera_calibrate.py
+++ camera_calibrate.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import glob
 
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
@@ -9,49 +10,52 @@ objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
-objpoints = []  # 3d point in real world space
-imgpoints = []  # 2d points in image plane.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
 
 nm = '/home/ivan/chessboard.mp4'
 nm = '/home/ivan/Desktop/webcam_callib.mp4'
+nm = '/home/ivan/Desktop/webcam_callib_320x240.mp4'
+
 
 cap = cv2.VideoCapture(nm)
 i = 0
 try:
     while cap.isOpened():
         ret, img = cap.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if ret:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            # Find the chess board corners
+            ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+            # If found, add object points, image points (after refining them)
 
-        # Find the chess board corners
-        _, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+            if ret:
+                objpoints.append(objp)
+                corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+                imgpoints.append(corners2)
+                print('Corners added')
 
-        # If found, add object points, image points (after refining them)
-        if ret == True:
-            objpoints.append(objp)
-            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
-            imgpoints.append(corners2)
-            print('Corners added')
-
-            # Draw and display the corners
-            img = cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
-            cv2.imshow('img', img)
-            cv2.waitKey(500)
-
+                # Draw and display the corners
+                img = cv2.drawChessboardCorners(img, (7,6), corners2,ret)
+                cv2.imshow('img', img)
+                cv2.waitKey(500)
         else:
-
             raise KeyboardInterrupt
 
         i += 1
+        print(i)
 
 
 except KeyboardInterrupt:
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    callib_data = [ret, mtx, dist, rvecs, tvecs]
+    if len(objpoints) > 0:
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+        callib_data = [ret, mtx, dist, rvecs, tvecs]
 
-    with open('callibration.txt', 'w') as writer:
-        for dt in callib_data:
-            print(dt)
-            writer.write('{} \n'.format(dt))
+        with open('callibration.txt', 'w') as writer:
+            for dt in callib_data:
+                print(dt)
+                writer.write('{} \n'.format(dt))
 
-    cv2.destroyAllWindows()
+
+cv2.destroyAllWindows()
 
