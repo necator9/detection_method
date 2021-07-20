da4cca7ae09f3c403462c98e47c915cd9c62da5c
Maksim Iushchenko <maxim@MaksimIushchenko.fila-network.de>
2021-07-20 09:33:02 +0200

    Reduce the maximal utilized disk space for logs

--- run_detection.py
+++ run_detection.py
@@ -59,8 +59,8 @@ check_if_dir_exists(out_dir)
 # Set up logging
 logger = logging.getLogger('detect')
 logger.setLevel(config['log_level'])
-file_handler = RotatingFileHandler(os.path.join(out_dir, 'detection.log'), mode='a', maxBytes=5 * 1024 * 1024,
-                                   backupCount=3)
+file_handler = RotatingFileHandler(os.path.join(out_dir, 'detection.log'), mode='a', maxBytes=4 * 1024 * 1024,
+                                   backupCount=2)
 ch = logging.StreamHandler()
 
 formatter = logging.Formatter('%(levelname)s %(asctime)s %(threadName)s - %(message)s')
